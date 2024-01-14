e302f241ce4d7040752ff1f93c885c13ab8dc412
Hokutonz <tonzofhoku@gmail.com>
2024-01-13 23:44:22 -0800

    Added more enmity to Sentinel, Rampart and Animated Flourish as well.

--- modules/hokutens/sql/enmity.sql
+++ modules/hokutens/sql/enmity.sql
@@ -1,6 +1,12 @@
 # Update Enmity on certain abilities
 # Provoke
 UPDATE `xidb`.`abilities` SET `CE`='500', `VE`='3000' WHERE  `abilityId`=35;
+# Sentinel
+UPDATE `xidb`.`abilities` SET `CE`='300', `VE`='1500' WHERE  `abilityId`=48;
+# Rampart
+UPDATE `xidb`.`abilities` SET `CE`='500', `VE`='1500' WHERE  `abilityId`=92;
+# Animated Flourish
+UPDATE `xidb`.`abilities` SET `CE`='300', `VE`='2500' WHERE  `abilityId`=204;
 
 # Update Enmity on certain spells
 # Flash
