a78b55d6dcdcc6c6c3458607777fac83b3b90ed9
Hokutonz <tonzofhoku@gmail.com>
2024-01-21 18:14:28 -0500

    Added custom quest Utils and Modules

---
+++ modules/hokutens/lua/quests/bastok/lb_master_of_puppets.lua
@@ -0,0 +1,195 @@
+-------------------------------------------------------
+-- Master of Puppets (Lv66)
+-------------------------------------------------------
+-- Steps
+-------------------------------------------------------
+-- !setvar [LB]MASTER_PUPPETS 0
+-- Kirk      !pos -286.2880 -12.0200 -83.6707 235
+-- Lars      !pos 461.4646 -34.1743 102.7589 79
+-- Ktulu     !pos 479.0318 -36.2500 106.8963 79
+-- Kirk      !pos -286.2880 -12.0200 -83.6707 235
+-- Unlocks: Lv71-75 (PUP LB5)
+-------------------------------------------------------
+-- !setvar [LB]MASTER_PUPPETS 6
+
+require("modules/module_utils")
+require('scripts/globals/utils')
+require('scripts/globals/player')
+require('scripts/globals/npc_util')
+local cq  = require("modules/hokutens/lua/system/utils/custom_quest")
+local cu  = require("modules/hokutens/lua/system/utils/custom_util")
+local cid = require("modules/hokutens/lua/system/utils/custom_id")
+-------------------------------------------------------
+local m = Module:new("lb_quest-master_of_puppets")
+
+local info =
+{
+    name   = "Master of Puppets",
+    author = "Tonzoffun",
+    var    = "[LB]MASTER_PUPPETS",
+    required =
+    {
+        item = { { 2333, 1 } },
+        name = "puppetmaster's testimony",
+    },
+    reward =
+    {
+        gil   = 500,
+        after = function(player)
+            player:setCharVar("[LB]PUP", 1)
+            if player:getLevelCap() < 75 then
+                player:setLevelCap(75);
+                player:PrintToPlayer("Your level cap has increased to 75.", xi.msg.channel.SYSTEM_3)
+            end
+            return true
+        end,
+    },
+}
+
+local KIRK       = "KIRK"
+local LARS       = "LARS"
+local SOULFLAYER = "SOULFLAYER"
+
+local entity =
+{
+    {
+        id     = KIRK,
+        name   = "Kirk",
+        type   = xi.objType.NPC,
+        look   = 3029,
+        area   = "Bastok_Markets",
+        pos    = { -286.2880, -12.0200, -83.6707, 89 }, -- !pos -286.2880 -12.0200 -83.6707 235
+        dialog =
+        {
+            NAME      = true,
+            DEFAULT   = { "I choose to live, not just exist..." },
+            START     =
+            {
+                { emote = xi.emote.WAVE },
+                "You there! You look like just the kind of adventurer I need.", 
+                { delay = 2000 },
+                string.format(" But first, bring me a %s to show me I can trust you.", info.required.name),
+            },
+            REMINDER  = { string.format("Bring me a %s to show me I can trust you.", info.required.name), },
+            DELIVERED =
+            {
+                { emote = xi.emote.PSYCH },
+                "I knew it! You're exactly who I hoped you would be!",
+                { delay = 1000 },
+                " I need you to help me seek and destroy my kind's sworn enemy.",
+                { delay = 2000 },
+                "So many puppets have been blackened and deactivated due to this menace.",
+                { emote = xi.emote.CRY },
+                { delay = 1000 },
+                " It's sad but true, so here comes revenge ...And justice for all!",
+                { delay = 2000 },
+                "As we speak, Lars, is on the front lines in Caedarva Mire, go back him up!",
+            },
+            REMINDER2 = { "Make haste to Caedarva Mire! Lars is waiting on the front lines to meet you!" },
+            FINISH    =
+            {
+                { emote = xi.emote.CHEER },
+                "Welcome home!! I thought this day would never come!",
+                { delay = 1000 },
+                " We can finally turn the page on that harvester of sorrow, all thanks to you.",
+                { delay = 2000 },
+                { emote = xi.emote.BOW },
+                "Wherever I may roam, I will never forget this kindness you have shown us.",
+                { delay = 1000 },
+                " You have earned this. Hopefully this gift will help fuel your road to ever greater power!",
+            },
+            AFTER     = { "The memory remains forever of your great victory, my dearest friend." },
+        },
+    },
+    {
+        id     = LARS,
+        name   = "Lars",
+        type   = xi.objType.NPC,
+        look   = 3030,
+        area   = "Caedarva_Mire",
+        pos    = { 461.4646, -34.1743, 102.7589, 28 }, -- !pos 461.4646 -34.1743 102.7589 79
+        dialog =
+        {
+            NAME     = true,
+            DEFAULT  = { "Boredom comes from a boring mind..." },
+            START    =
+            {
+                { emote = xi.emote.HURRAY },
+                "Our savior has arrived!! Now we can fight fire with fire!",
+                { delay = 1000 },
+                " If darkness had a son, it would answer to the call of Ktulu.",
+                { delay = 2000 },
+                { emote = xi.emote.KNEEL },
+                "Please save our kind from this creeping death before my battery dies and I disappear.",
+                { delay = 1000 },
+                " Nothing else matters until it sleeps and fades to black.",
+            },
+            HELP     = { "Will you stand and fight against this unforgiven destroyer of our race?" },
+            SPAWN    = { "There it is!! The one that has been killing all my brethren!" },
+            FINISH   =
+            {
+                { emote = xi.emote.CLAP },
+                "YOU DID IT! We have not known peace for a long 72 seasons.",
+                { delay = 1000 },
+                " I can't believe its reign is finally over.",
+                { delay = 2000 },
+                "You must be the one to bring the good tidings to Kirk!",
+            },
+            REMINDER = { "Bring the good tidings to Kirk!" },
+        },
+    },
+    {
+        id          = SOULFLAYER,
+        name        = "Ktulu",
+        type        = xi.objType.MOB,
+        look        = "0x0000ef0600000000000000000000000000000000",
+        groupId     = 58,
+        groupZoneId = 79,
+        area        = "Caedarva_Mire",
+        pos         = { 479.0318, -36.2500, 106.8963, 101 }, -- !pos 479.0318 -36.2500 106.8963 79
+        level       = 68,
+    },
+}
+
+local step =
+{
+    {
+        check  = cq.checks({ level = 66, job = xi.job.PUP }),
+        [KIRK] = cq.talkStep("START", info.name),
+    },
+    {
+        [KIRK] =
+        {
+            onTrigger = cq.talkOnly("REMINDER"),
+            onTrade   = cq.tradeStep("DELIVERED", "REMINDER", info.required.item),
+        }
+    },
+    {
+        [KIRK] = cq.talkOnly("REMINDER2"),
+        [LARS] = cq.talkStep("START"),
+    },
+    {
+        check        = cq.checks({ level = 66, job = xi.job.PUP }),
+        [LARS]       = cq.menuSpawn(SOULFLAYER, { "HELP", "Agree to help?" }, { { "Not yet." }, { "I'm ready.", "SPAWN" } }, 2, false, { levelCap = 70, partySize = 1, subjob = false }),
+        [SOULFLAYER] = cq.killStep(),
+    },
+    {
+        [LARS]   = cq.talkStep("FINISH"),
+    },
+    {
+        [LARS] = cq.talkOnly("REMINDER"),
+        [KIRK] = cq.giveStep("FINISH", info.reward, info.name, cq.music.BASTOK),
+    },
+    {
+        check  = cq.checks({ job = xi.job.PUP }),
+        [KIRK] = cq.talkOnly("AFTER"),
+    }
+}
+
+cq.add(m, {
+    info   = info,
+    entity = entity,
+    step   = step,
+})
+
+return m
---
+++ modules/hokutens/lua/quests/jeuno/lb_daddy_issues.lua
@@ -0,0 +1,212 @@
+-------------------------------------------------------
+-- Daddy Issues (Lv66)
+-------------------------------------------------------
+-- Steps
+-------------------------------------------------------
+-- !setvar [LB]DADDY_ISSUES 0
+-- Brittany      !pos -56.8856 0.0001 99.0318 244
+-- Mindy         !pos 480.5514 0.1380 -153.2160 82
+-- Daddy         !pos 466.2906 -0.9709 -149.1866 82
+-- Brittany      !pos -56.8856 0.0001 99.0318 244
+-- Unlocks: Lv71-75 (DNC LB5)
+-------------------------------------------------------
+-- !setvar [LB]DADDY_ISSUES 6
+
+require("modules/module_utils")
+require('scripts/globals/utils')
+require('scripts/globals/player')
+require('scripts/globals/npc_util')
+local cq  = require("modules/hokutens/lua/system/utils/custom_quest")
+local cu  = require("modules/hokutens/lua/system/utils/custom_util")
+local cid = require("modules/hokutens/lua/system/utils/custom_id")
+-------------------------------------------------------
+local m = Module:new("lb_quest-daddy_issues")
+
+local info =
+{
+    name   = "Daddy Issues",
+    author = "Tonzoffun",
+    var    = "[LB]DADDY_ISSUES",
+    required =
+    {
+        item = { { 2556, 1 } },
+        name = "dancer's testimony",
+    },
+    reward =
+    {
+        gil   = 500,
+        after = function(player)
+            player:setCharVar("[LB]DNC", 1)
+            if player:getLevelCap() < 75 then
+                player:setLevelCap(75);
+                player:PrintToPlayer("Your level cap has increased to 75.", xi.msg.channel.SYSTEM_3)
+            end
+            return true
+        end,
+    },
+}
+
+local BRITTANY = "BRITTANY"
+local MINDY    = "MINDY"
+local MALBORO  = "MALBORO"
+
+local entity =
+{
+    {
+        id     = BRITTANY,
+        name   = "Brittany",
+        type   = xi.objType.NPC,
+        look   = cu.lookTable({
+            race = xi.race.HUME_F,
+            face = cid.face.B2,
+            head = cid.model.STARLET,
+            body = cid.model.STARLET,
+            hand = cid.model.STARLET,
+            legs = cid.model.STARLET,
+            feet = cid.model.STARLET,
+        }),
+        area      = "Upper_Jeuno",
+        pos       = { -56.8856, 0.0001, 99.0318, 181 }, -- !pos -56.8856 0.0001 99.0318 244
+        dialog    =
+        {
+            NAME      = true,
+            DEFAULT   = { "Don't I totally look cute?!" },
+            START     =
+            {
+                { emote = xi.emote.CHEER },
+                "Hey there!! Oooomg!! So I totally like just heard of a like totally scary story!",
+                { delay = 1000 },
+                string.format(" If you bring me a %s, I'll like totally tell you all about it!!", info.required.name),
+            },
+            REMINDER  = { string.format("Bring me a %s, then I'll like totally tell you the story I heard.", info.required.name), },
+            DELIVERED =
+            {
+                { emote = xi.emote.HURRAY },
+                "Yay!! You totally did it! Okaaaaay.. so like my friend Bethany, not Brittany, that's totally me, silly!",
+                { delay = 1000 },
+                " She like totally told me about this scary story that happened years ago to a girl who was totally just like us!",
+                { delay = 2000 },
+                { emote = xi.emote.PANIC },
+                "Bethany said that like this girl had to fight a crazy scary monster! It was huge and green and had terrible breath!",
+                { delay = 1000 },
+                " Isn't that gross! OMG I totally would freak out if that happened to me!",
+                { delay = 2000 },
+                "She said it happened in some like jungle place.. Jugner Forest I think, but like yeeears ago!",
+            },
+            REMINDER2 = { "Can you believe that like happened in Jugner Forest so totally long ago!??" },
+            FINISH    =
+            {
+                { emote = xi.emote.SHOCKED },
+                "Heeey! Long time no see!! OMG what!??? You met her!?? What do you mean?",
+                { delay = 1000 },
+                " That like doesn't even make sense, but whaaaaaatever floats your boat I guess.",
+                { delay = 2000 },
+                { emote = xi.emote.BLUSH },
+                "I like totally missed you while you were gone, if I give you a present will you stay here with me??",
+                { delay = 1000 },
+                " Here.. like you can totally have this. Do you like want to watch me dance?",
+            },
+            AFTER     = { "Thanks for like totally staying to cheer me on!" },
+        },
+    },
+    {
+        id     = MINDY,
+        name   = "Mindy",
+        type   = xi.objType.NPC,
+        look   = cu.lookTable({
+            race = xi.race.HUME_F,
+            face = cid.face.A8,
+            head = cid.model.ETOILE,
+            body = cid.model.ETOILE,
+            hand = cid.model.ETOILE,
+            legs = cid.model.ETOILE,
+            feet = cid.model.ETOILE,
+        }),
+        area   = "Jugner_Forest_[S]",
+        pos    = { 480.5514, 0.1380, -153.2160, 86 }, -- !pos 480.5514 0.1380 -153.2160 82
+        dialog =
+        {
+            NAME     = true,
+            DEFAULT  = { "Hiiiiii! Don't I have like the best dance moves you've ever seen??" },
+            START    =
+            {
+                { emote = xi.emote.SHOCKED },
+                "Oh hi! I like totally didn't see you there.",
+                { delay = 1000 },
+                " I have no idea how I made it all the way out here, I just kept practicing my dance moves.",
+                { delay = 2000 },
+                { emote = xi.emote.HUH },
+                "Next thing I knew I was like way out here and now I'm like totally lost!!",
+                { delay = 1000 },
+                " OMG!! Did you hear that?? What was that!!??",
+
+            },
+            HELP     = { "Can you like totally help me look around for whatever made that sound??" },
+            SPAWN    = { "Eeeeeeeek!! Kill it!! I'm too pretty to die this young!" },
+            FINISH   =
+            {
+                { emote = xi.emote.BLUSH },
+                "You totally just like saved my life. I like totally owe you!!",
+                { delay = 1000 },
+                " But can you do me a huge favor??? Like can we just agree that I totally like killed it myself?",
+                { delay = 2000 },
+                "What?? You come from the future? OMG! So you can totally check and see if they heard about how I killed it!",
+            },
+            REMINDER = { "I totally hope this story reaches dancers in the future and they like know how brave I was." },
+        },
+    },
+    {
+        id          = MALBORO,
+        name        = "Daddy",
+        type        = xi.objType.MOB,
+        look        = "0x00007c0100000000000000000000000000000000",
+        groupId     = 13,
+        groupZoneId = 90,
+        area        = "Jugner_Forest_[S]",
+        pos         = { 466.2906, -0.9709, -149.1866, 28 }, -- !pos 466.2906 -0.9709 -149.1866 82
+        level       = 68,
+    },
+}
+
+local step =
+{
+    {
+        check      = cq.checks({ level = 66, job = xi.job.DNC }),
+        [BRITTANY] = cq.talkStep("START", info.name),
+    },
+    {
+        [BRITTANY] =
+        {
+            onTrigger = cq.talkOnly("REMINDER"),
+            onTrade   = cq.tradeStep("DELIVERED", "REMINDER", info.required.item),
+        }
+    },
+    {
+        [BRITTANY] = cq.talkOnly("REMINDER2"),
+        [MINDY]    = cq.talkStep("START"),
+    },
+    {
+        check     = cq.checks({ level = 66, job = xi.job.DNC }),
+        [MINDY]   = cq.menuSpawn(MALBORO, { "HELP", "Agree to help?" }, { { "Not yet." }, { "I'm ready.", "SPAWN" } }, 2, false, { levelCap = 70, partySize = 1, subjob = false }),
+        [MALBORO] = cq.killStep(),
+    },
+    {
+        [MINDY]   = cq.talkStep("FINISH"),
+    },
+    {
+        [MINDY]    = cq.talkOnly("REMINDER"),
+        [BRITTANY] = cq.giveStep("FINISH", info.reward, info.name, cq.music.JEUNO),
+    },
+    {
+        check      = cq.checks({ job = xi.job.DNC }),
+        [BRITTANY] = cq.talkOnly("AFTER"),
+    }
+}
+
+cq.add(m, {
+    info   = info,
+    entity = entity,
+    step   = step,
+})
+
+return m
